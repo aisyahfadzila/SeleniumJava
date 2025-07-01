8c49e0f2a09bc23d5a34e784d10800c25dba1837
Aisyah Fadzila Hani <aisyahfadzila@student.telkomuniversity.ac.id>
2025-07-01 19:02:21 +0700

    SauceDemo Login Test

---
+++ src/main/java/config/env_target.java
@@ -0,0 +1,8 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+
+public class env_target {
+    protected WebDriver driver;
+    protected String baseURL = "https://www.saucedemo.com/";
+}
