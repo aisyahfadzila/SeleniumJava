6d721adafd7c803f0aa0e5bd9b120e31122423aa
Aisyah Fadzila Hani <aisyahfadzila@student.telkomuniversity.ac.id>
2025-07-01 19:06:23 +0700

    SauceDemo Login Test

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -48,4 +48,24 @@ public class Login extends env_target{
         );
         driver.quit();
     }
+
+    @Test
+    public void invalidPassword(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(baseURL);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@data-test=\"username\"]"))
+        );
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauces");
+        driver.findElement(By.xpath("//input[@type=\"submit\"][@data-test=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"))
+        );
+        driver.quit();
+    }
 }
